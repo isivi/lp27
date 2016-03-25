3ce2c6eabe995f2ece31d090c850171c77ed3dcf
Konrad Słoniewski <konrad.sloniewski@gmail.com>
2016-03-25 12:41:20 +0100

    Fixed view name

--- lp27/lp27_app/urls.py
+++ lp27/lp27_app/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import home, no_stats, stats
 
 urlpatterns = [
-    url(r'^$', home),
+    url(r'^$', home, name='home'),
 
     # Stats
     url(r'^nostats/$', no_stats, name='no_stats'),
