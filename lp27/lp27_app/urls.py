0311a9625dd922c533b0471e5bf45eec51ab8467
Konrad Słoniewski <konrad.sloniewski@gmail.com>
2016-03-29 17:20:49 +0200

    Landing page different versions stats divided by url

--- lp27/lp27_app/urls.py
+++ lp27/lp27_app/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from .views import home, no_stats, stats
+from .views import home, recruiter, algorithm, no_stats, stats
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^recruiter/$', recruiter, name='recruiter'),
+    url(r'^algorithm/$', algorithm, name='algorithm'),
 
     # Stats
     url(r'^nostats/$', no_stats, name='no_stats'),
--- lp27/lp27_app/views.py
+++ lp27/lp27_app/views.py
@@ -16,11 +16,46 @@ def home(request):
     else:
         landing_version_a = str_to_bool(landing_version_a)
 
-    template = 'lp27_app/home.html'
-    context = {'landing_version_a': landing_version_a}
-    response = render_to_response(template, context, context_instance=RequestContext(request))
-    set_cookie(response, 'landing_version_a', landing_version_a)
-    return response
+    if landing_version_a:
+        return redirect('recruiter')
+    else:
+        return redirect('algorithm')
+
+
+def recruiter(request):
+    landing_version_a = request.COOKIES.get('landing_version_a', None)
+
+    if landing_version_a is None:
+        landing_version_a = True
+    else:
+        landing_version_a = str_to_bool(landing_version_a)
+
+    if landing_version_a:
+        template = 'lp27_app/home.html'
+        context = {'landing_version_a': landing_version_a}
+        response = render_to_response(template, context, context_instance=RequestContext(request))
+        set_cookie(response, 'landing_version_a', landing_version_a)
+        return response
+    else:
+        return redirect('algorithm')
+
+
+def algorithm(request):
+    landing_version_a = request.COOKIES.get('landing_version_a', None)
+
+    if landing_version_a is None:
+        landing_version_a = False
+    else:
+        landing_version_a = str_to_bool(landing_version_a)
+
+    if landing_version_a:
+        return redirect('recruiter')
+    else:
+        template = 'lp27_app/home.html'
+        context = {'landing_version_a': landing_version_a}
+        response = render_to_response(template, context, context_instance=RequestContext(request))
+        set_cookie(response, 'landing_version_a', landing_version_a)
+        return response
 
 
 # Stats
