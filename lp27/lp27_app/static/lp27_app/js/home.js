3331382571872ce4237cbe9c17bf8dd761ff4446
Konrad Słoniewski <konrad.sloniewski@gmail.com>
2016-04-14 16:57:51 +0200

    Scrolling to first block

--- lp27/lp27_app/static/lp27_app/js/home.js
+++ lp27/lp27_app/static/lp27_app/js/home.js
@@ -1,2 +1,10 @@
 $(function() {
+  var scrollToFirstBlock = function() {
+    $('html, body').animate({
+      scrollTop: $(".block1").offset().top
+    }, 1000);
+  };
+
+  $(".how-it-works").click(scrollToFirstBlock);
+  $(".arrow-down").click(scrollToFirstBlock);
 });
