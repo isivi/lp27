1b350be4ee195feae84b494c81928135a86a6cbf
Konrad Słoniewski <konrad.sloniewski@gmail.com>
2016-04-15 12:35:06 +0200

    Links in the site footer

--- lp27/lp27_app/static/lp27_app/js/home.js
+++ lp27/lp27_app/static/lp27_app/js/home.js
@@ -5,6 +5,13 @@ $(function() {
     }, 1000);
   };
 
+  var scrollToFormBlock = function() {
+    $('html, body').animate({
+      scrollTop: $(".block4").offset().top
+    }, 1000);
+  };
+
   $(".how-it-works").click(scrollToFirstBlock);
+  $(".go-to-form").click(scrollToFormBlock);
   $(".arrow-down").click(scrollToFirstBlock);
 });
